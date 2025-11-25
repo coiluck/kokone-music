2a957133080a5c967fcd8119ce29634f4e763f7e
coiluck <wvy.wingle@gmail.com>
2025-11-26 08:22:03 +0900

    Add: color select logic

--- src/renderer/src/settings.js
+++ src/renderer/src/settings.js
@@ -8,7 +8,25 @@ export async function applySettings() {
     const selectedFont = e.target.value;
     applyFont(selectedFont);
   });
-
+  // color用
+  const userColors = await getStoreColors();
+  applyColors(userColors);
+  ['accent', 'text', 'bg'].forEach(key => {
+    const input = document.querySelector(`input[name="${key}"]`);
+    input.value = userColors[key];
+    updateColorLabel(input, userColors[key]);
+    input.addEventListener('input', (e) => {
+      const newValue = e.target.value;
+      document.documentElement.style.setProperty(`--${key}`, newValue);
+      updateColorLabel(input, newValue);
+    });
+    input.addEventListener('change', async (e) => {
+      const newValue = e.target.value;
+      const currentColors = await getStoreColors();
+      currentColors[key] = newValue;
+      await window.settings.set('colors', currentColors);
+    });
+  });
   // 下の関数を集めておく
 }
 
@@ -20,4 +38,24 @@ async function getStoreFont() {
 function applyFont(fontValue) {
   document.documentElement.style.setProperty('--font', fontValue);
   window.settings.set('font', fontValue);
+}
+
+// color
+async function getStoreColors() {
+  const defaultColors = {
+    accent: '#ff7f7e',
+    text: '#fff3f1',
+    bg: '#0a0f1e'
+  };
+  const savedColors = await window.settings.get('colors');
+  return savedColors || defaultColors;
+}
+function applyColors(colors) {
+  document.documentElement.style.setProperty('--accent', colors.accent);
+  document.documentElement.style.setProperty('--text', colors.text);
+  document.documentElement.style.setProperty('--bg', colors.bg);
+}
+function updateColorLabel(inputElement, value) {
+  const label = inputElement.parentElement.querySelector('.setting-color-value');
+  label.textContent = value;
 }
